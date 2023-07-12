ab53b5b2f644256cb3ebde32e8699e2e40ef733f
erinelise2 <erinelise2@gmail.com>
2023-07-12 08:08:45 -0700

    added old code notes from previous form of this idea to turn into react app

--- src/Components/ObservationForm.js
+++ src/Components/ObservationForm.js
@@ -36,55 +36,104 @@ export default function postNewObservation(e) {
             <input onChange={(e) => setNewTimeOfDay(e.target.value)}></input><br></br>
             <button onClick={(e) => postNewObservation(e)}>Submit</button>
           </form>
-          <br></br>
-    
-    
-          {observations.map((observation, index) => (
-            <div className="mapContainer" key={index}>
-              <div>
-                Observation: {observation.observation} <br></br>
-                Classification: {observation.classification} <br></br>
-                Location: {observation.location} <br></br>
-                Location Type: {observation.locationType} <br></br>
-                Date: {observation.date} <br></br>
-                Time Of Day: {observation.timeofday} <br></br>
-                <button onClick={() => deleteObservation(observation.id)}>🗑</button>
-              </div>
-              <form>
-                <label>Edit Observation</label>
-                <input
-                  onChange={(e) => setUpdatedObservation(e.target.value)}
-                ></input>
-                <br></br>
-                <label>Edit Classification</label>
-                <input
-                  onChange={(e) => setUpdatedClassification(e.target.value)}
-                ></input>
-                <br></br>
-                <label>Edit Location</label>
-                <input
-                  onChange={(e) => setUpdatedLocation(e.target.value)}
-                ></input>
-                <br></br>
-                <label>Edit Location Type</label>
-                <input
-                  onChange={(e) => setUpdatedLocationType(e.target.value)}
-                ></input>
-                <br></br>
-                <label>Edit Date</label>
-                <input
-                  onChange={(e) => setUpdatedDate(e.target.value)}
-                ></input>
-                <br></br>
-                <label>Edit Time Of Day</label>
-                <input
-                  onChange={(e) => setUpdatedTimeOfDay(e.target.value)}
-                ></input>
-                <br></br>
-                <button onClick={(e) => updateObservation(e, observation)}>Update</button>
-              </form>
-            </div>
-          ))}
-        </div>
+          <br></br>    
+         </div>
     )
   }
+
+  // html form from week 10 with the radio buttons I want to add to this, if possible
+  //                <form>
+  //                       <h3 class='header m-2'><span class="border-bottom row border-3 p-1 fs-1">What Did You See?</span></h3>
+  //                       <div class='taskForm form-group m-4 fs-4'>
+  //                           <div>
+  //                               <label class="mb-2 fw-bold" for="newObservation">Name of Animal or Insect:</label>
+  //                               <input type="text" class="form-control fs-5" id="newObservation" placeholder="Name of Animal or Insect" />
+  //                           </div>
+  //                           <div>
+  //                               <label class="mt-3 mb-2 fw-bold">Classifcation: </label><br>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2" id="button1" type="radio" name="classoptradio" value="Mammal" checked /> Mammal
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2" id="button2" type="radio" name="classoptradio" value="Reptile" /> Reptile
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2" id="button3" type="radio" name="classoptradio" value="Amphibian" /> Amphibian
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2" id="button4" type="radio" name="classoptradio" value="Bird" /> Bird
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2" id="button5" type="radio" name="classoptradio" value="Bug, Insect or Spider" /> Bug, Insect or Spider
+  //                               </label>
+  //                          </div>
+  //                           <div>
+  //                               <label class="mt-2 mb-2 fw-bold" for="newLocation">Location Observed:</label>
+  //                               <input type="text" class="form-control fs-5" id="newLocation" placeholder="City, State"/>
+  //                           </div>
+  //                           <div>
+  //                               <label class="mt-3 mb-2 fw-bold">Type of Location: </label><br>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-1" id="radioButton1" type="radio" name="locationoptradio" value="Desert" checked /> Desert
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-1" id="radioButton2" type="radio" name="locationoptradio" value="Field or Meadow" /> Field or Meadow
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-1" id="radioButton3" type="radio" name="locationoptradio" value="Forest or Woods" /> Forest or Woods
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-1" id="radioButton4" type="radio" name="locationoptradio" value="Hill or Mountain" /> Hill or Mountain
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2" id="radioButton5" type="radio" name="locationoptradio" value="Pond, Lake, or River" /> Pond, Lake, or River
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2" id="radioButton6" type="radio" name="locationoptradio" value="Ocean" /> Ocean
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-1" id="radioButton7" type="radio" name="locationoptradio" value="Beach" /> Beach
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-1" id="radioButton8" type="radio" name="locationoptradio" value="Sky" /> Sky
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-1" id="radioButton9" type="radio" name="locationoptradio" value="Neighborhood or City" /> Neighborhood or City 
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-1" id="radioButton10" type="radio" name="locationoptradio"  value="State or National Park" /> State or National Park
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-1" id="radioButton11" type="radio" name="locationoptradio" value="Zoological Society" /> Zoological Society
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2" id="radioButton11" type="radio" name="locationoptradio" value="Other" /> Other 
+  //                                   <!-- <input type="text" class="form fs-6 fst-italic" id="newLocation" placeholder="Describe Location"/> -->
+  //                               </label>
+  //                           </div>
+  //                           <div>
+  //                               <label class="mt-3 mb-2 fw-bold" for="newDate">Observation Date:</label>
+  //                               <input class="form-control" id="newDate" type="Date" />
+  //                           </div>
+  //                           <div>
+  //                               <label class="mt-2 mb-2 fw-bold">Time of Day: </label><br>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2" id="daybutton1" type="radio" name="timeoptradio" value="Morning" checked /> Morning
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2" id="daybutton2" type="radio" name="timeoptradio" value="Day" /> Day
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2"id="daybutton3" type="radio" name="timeoptradio" value="Evening" /> Evening
+  //                               </label>
+  //                               <label class="radio fs-5">
+  //                                   <input class="m-2" id="daybutton4" type="radio" name="timeoptradio" value="Night" /> Night
+  //                               </label>
+  //                          </div>
+  //                       </div>
+  //                           <div class="">
+  //                           <button class='btn btn-sm new-btn border border-3 rounded-pill ms-4 mb-4 p-2.5 fw-bolder text-white fs-5' onclick="addObservation()" id="submitObservation">Add Observation To Your Field Notes</button>
+  //                           </div>
+  //                       </div>
+  //                   </form>
+  
